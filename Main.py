e3f4ac4186e18fd0319339ef2a7ec60ec08843f9
Flemming von Seht <f.v.seht@icloud.com>
2024-02-13 19:25:08 +0100

    try stuff

--- Main.py
+++ Main.py
@@ -553,7 +553,7 @@ def handle_button_input(pos_new: int, pos_old: int) -> int:
             or type(pos_old) is not int
             or type(player_nr) is not int):
         return 1
-    while data[pos_new] or data[pos_new-1]:
+    while data[pos_new] or data[pos_new+1]:
         pos_new += 2
     pos_new = pos_new + player_nr
     change_active_position(pos_new, pos_old)
