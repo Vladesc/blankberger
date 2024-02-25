e7ef48ed4545141873971313ea524e1f84f9ab77
Flemming von Seht <f.v.seht@icloud.com>
2024-02-25 23:53:54 +0100

    beautify the current code --> untested

--- Constants.py
+++ Constants.py
@@ -3,11 +3,33 @@ from typing import Final
 WINDOW_HEIGHT: Final = 480
 WINDOW_WIDTH: Final = 800
 WINDOW_TITLE: Final = "4 Gewinnt"
+WINDOW_TITLE_RUNNING_GAME: Final = "Sie spielen gerade 4 Gewinnt"
 
 GAME_MODE_LABEL: Final = "Spielmodus wählen"
 GAME_WELCOME_LABEL: Final = "Willkommen bei 4 Gewinnt. Bitte wählen sie ihr Spiel."
 GAME_PLAYER_1_LABEL: Final = "Spieler 1: "
 GAME_PLAYER_2_LABEL: Final = "Spieler 2: "
 
+GAME_PLAYER_1_PLACEHOLDER:Final = "Spieler 1"
+GAME_PLAYER_2_PLACEHOLDER:Final = "Spieler 2"
+
+GAME_MODE_PVE_LABEL: Final = "Spieler gegen Computer"
+GAME_MODE_PVP_LABEL: Final = "Spieler gegen Spieler"
+GAME_MODE_PVP_DIFFICULTY_CHOOSE: Final = "Wählen sie die Schwierigkeit aus:"
+GAME_MODE_PVP_DIFFICULTY_EASY: Final = "Leicht"
+GAME_MODE_PVP_DIFFICULTY_MEDIUM: Final = "Mittel"
+GAME_MODE_PVP_DIFFICULTY_HARD: Final = "Schwer"
+
+GAME_SOUND_LABEL: Final = "Sound: "
+GAME_SOUND_LABEL_ON: Final = "On"
+GAME_SOUND_LABEL_OFF: Final = "Off"
+
+GAME_INFO_BUTTON: Final = "Info"
+GAME_RULES_BUTTON: Final = "Spielregeln"
+GAME_START_BUTTON: Final = "Spiel Starten"
 GAME_SHUTDOWN_BUTTON: Final = "Shutdown System"
 GAME_END_BUTTON: Final = "Beenden"
+
+GAME_PVE_MSG_MISSING_NAME: Final = "Spieler 1 hat keinen Namen."
+GAME_PVP_MSG_MISSING_NAME: Final = "Es sind nicht für alle Spieler Namen eingetragen."
+
--- GUI.py
+++ GUI.py
@@ -28,12 +28,14 @@ class GUI(object):
         self.sound_state_container = IntVar()
 
         self.main_window()
-
-        # in der Eingabeschleife auf die Eingabe durch den Benutzer warten.
         self.fenster.mainloop()
 
-    def main_window(self):
-        def action_get_info_dialog():
+    def main_window(self) -> None:
+        """
+        Generate initial main window.
+        :return: None
+        """
+        def action_get_info_dialog() -> None:
             m_text = "\
                 ************************\n\
                 Autoren: OFR Sonnberger und OFRzS Blank\n\
@@ -42,7 +44,7 @@ class GUI(object):
                 ************************"
             messagebox.showinfo(message=m_text, title="Infos")
 
-        def action_get_rules_dialog():
+        def action_get_rules_dialog() -> None:
             m_text = "\
                 ************************\n\
                 Spielregeln für 4 Gewinnt\n\
@@ -51,7 +53,11 @@ class GUI(object):
                 ************************"
             messagebox.showinfo(message=m_text, title="Spielregeln")
 
-        def build_grid():
+        def build_grid() -> None:
+            """
+            Set position of gui elements inside the grid.
+            :return: None
+            """
             willkommen_label.grid(row=0, column=1)
 
             spielmodus_label.grid(row=1, column=1)
@@ -74,21 +80,33 @@ class GUI(object):
             end_application_button.grid(row=14, column=1)
             shutdown_system_button.grid(row=14, column=2)
 
-        def reset_game_container_values():
+        def reset_game_container_values() -> None:
+            """
+            Reset data container values for game mode, game difficulty and sound.
+            :return: None
+            """
             self.game_mode_container.set(0)
             self.game_difficulty_container.set(0)
             self.sound_state_container.set(0)
 
-        def ctrl_sound_state_change():
+        def ctrl_sound_state_change() -> None:
+            """
+            Change the value of the sound state data container "sound_state_container".
+            :return: None
+            """
             if self.sound_state_container.get() == 0:
-                print("Es sollte Musik laufen!")
+                print("Es sollte Musik laufen!")  # todo rm debug msg
                 # pygame.mixer.music.play(-1) #todo SOUND STUFF
                 # pygame.mixer.music.set_volume(.5) #todo SOUND STUFF
             else:
-                print("Jetzt läuft keine Musik!")
+                print("Jetzt läuft keine Musik!")  # todo rm debug msg
                 pygame.mixer.music.stop()
 
-        def ctrl_game_mode_change():
+        def ctrl_game_mode_change() -> None:
+            """
+            Change the value of the game mode data container "game_mode_container".
+            :return: None
+            """
             if self.game_mode_container.get() == 0:
                 spieler1_label.grid(row=3, column=0)
                 self.spieler1_eingabefeld.grid(row=3, column=1)
@@ -119,80 +137,97 @@ class GUI(object):
         self.spieler1_eingabefeld = Entry(self.fenster, bd=5, width=40)
         self.spieler2_eingabefeld = Entry(self.fenster, bd=5, width=40)
 
-        self.spieler1_eingabefeld.insert(0, "Spieler 1")
-        self.spieler2_eingabefeld.insert(0, "Spieler 2")
+        self.spieler1_eingabefeld.insert(0, Constants.GAME_PLAYER_1_PLACEHOLDER)
+        self.spieler2_eingabefeld.insert(0, Constants.GAME_PLAYER_2_PLACEHOLDER)
 
         radio_oneOnOne = Radiobutton(self.fenster,
-                                     text="Spieler gegen Spieler",
+                                     text=Constants.GAME_MODE_PVP_LABEL,
                                      padx=20,
                                      variable=self.game_mode_container,
                                      command=ctrl_game_mode_change,
                                      value=0)
         radio_oneOnCpu = Radiobutton(self.fenster,
-                                     text="Spieler gegen Computer",
+                                     text=Constants.GAME_MODE_PVE_LABEL,
                                      padx=20,
                                      variable=self.game_mode_container,
                                      command=ctrl_game_mode_change,
                                      value=1)
 
-        cpuLevel_Label = Label(self.fenster, text="Wählen sie die Schwierigkeit aus:")
+        cpuLevel_Label = Label(self.fenster, text=Constants.GAME_MODE_PVP_DIFFICULTY_CHOOSE)
 
         radio_leicht = Radiobutton(self.fenster,
-                                   text="Leicht",
+                                   text=Constants.GAME_MODE_PVP_DIFFICULTY_EASY,
                                    padx=20,
                                    variable=self.game_difficulty_container,
                                    value=0)
         radio_mittel = Radiobutton(self.fenster,
-                                   text="Mittel",
+                                   text=Constants.GAME_MODE_PVP_DIFFICULTY_MEDIUM,
                                    padx=20,
                                    variable=self.game_difficulty_container,
                                    value=1)
         radio_schwer = Radiobutton(self.fenster,
-                                   text="Schwer",
+                                   text=Constants.GAME_MODE_PVP_DIFFICULTY_HARD,
                                    variable=self.game_difficulty_container,
                                    padx=20,
                                    value=2)
 
-        start_button = Button(self.fenster, text="Spiel starten", command=self.start_game_instance)
+        start_button = Button(self.fenster, text=Constants.GAME_START_BUTTON, command=self.start_game_instance)
 
-        sound_Label = Label(self.fenster, text="Sound: ")
-        radio_sound_on = Radiobutton(self.fenster, text="On", padx=20, variable=self.sound_state_container,
+        sound_Label = Label(self.fenster, text=Constants.GAME_SOUND_LABEL)
+        radio_sound_on = Radiobutton(self.fenster, text=Constants.GAME_SOUND_LABEL_ON, padx=20, variable=self.sound_state_container,
                                      value=0, command=ctrl_sound_state_change)
-        radio_sound_off = Radiobutton(self.fenster, text="Off", padx=20, variable=self.sound_state_container,
+        radio_sound_off = Radiobutton(self.fenster, text=Constants.GAME_SOUND_LABEL_OFF, padx=20, variable=self.sound_state_container,
                                       value=1, command=ctrl_sound_state_change)
 
-        spielregeln_button = Button(self.fenster, text="Spielregeln", command=action_get_rules_dialog)
-        info_button = Button(self.fenster, text="Info", command=action_get_info_dialog)
+        spielregeln_button = Button(self.fenster, text=Constants.GAME_RULES_BUTTON, command=action_get_rules_dialog)
+        info_button = Button(self.fenster, text=Constants.GAME_INFO_BUTTON, command=action_get_info_dialog)
         end_application_button = Button(self.fenster, text=Constants.GAME_END_BUTTON, command=self.fenster.quit)
         shutdown_system_button = Button(self.fenster, text=Constants.GAME_SHUTDOWN_BUTTON,
                                         command=self.action_shutdown_system)
 
-        # Set Element Position
         build_grid()
-
-        # Fenster während des Spiels
         ctrl_sound_state_change()
         reset_game_container_values()
 
-    def game_window(self): ## todo GUI Schick machen... Schrift größer, Hintergrundfarbe f. Spieler setzen
-        def action_end_game():
+    def game_window(self) -> None:
+        """
+        Generate and set the window content of a running game instance.
+        Start the game instance thread and react on player actions.
+        :return: None
+        """
+
+        def action_end_game() -> None:
+            """
+            Stop the running game instance thread and close the window
+            :return: None
+            """
             self.game_instance.stop()
             spiele_fenster.quit()
 
-        def change_active_player(current_player: int):
+        def change_active_player(current_player: int) -> None:
+            """
+            Change the display (window content) for current player.
+            :param current_player: player for the next turn.
+            :return: None
+            """
             if not current_player:
                 spieler_name_anzeigen_label2.grid_forget()
                 spieler_name_anzeigen_label1.grid(row=0, column=0)
             else:
                 spieler_name_anzeigen_label1.grid_forget()
                 spieler_name_anzeigen_label2.grid(row=0, column=0)
-        def close_top_window():
+
+        def close_top_window() -> None:
+            """
+            Close the running game window and show the main menu window.
+            :return: None
+            """
             spiele_fenster.destroy()
 
         spiele_fenster = tkinter.Toplevel(self.fenster)
-        spiele_fenster.title("Sie spielen gerade 4 Gewinnt")
+        spiele_fenster.title(Constants.WINDOW_TITLE_RUNNING_GAME)
         spiele_fenster.geometry("%dx%d+0+0" % (Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT))
-        beenden_button1 = Button(spiele_fenster, text="Beenden", command=action_end_game)
+        beenden_button1 = Button(spiele_fenster, text=Constants.GAME_END_BUTTON, command=action_end_game)
         spieler_name_anzeigen_label1 = Label(spiele_fenster,
                                              text="Spieler " + self.spieler1_eingabefeld.get() + " ist dran")
         spieler_name_anzeigen_label2 = Label(spiele_fenster,
@@ -207,26 +242,29 @@ class GUI(object):
         self.active_game_thread.start()
 
     def start_game_instance(self):
-        print("Spiel wird gestartet!")
         spieler_namen = (self.spieler1_eingabefeld.get(), self.spieler2_eingabefeld.get())
-        print("Spieler1: ", spieler_namen[0])
-        print("Spieler2: ", spieler_namen[1])
         if self.game_mode_container.get() == 0:
             if (len(spieler_namen[0]) > 0) & (len(spieler_namen[1]) > 0):
                 self.game_window()
             else:
-                messagebox.showwarning(message="Es sind nicht für alle Spieler Namen eingetragen")
+                messagebox.showwarning(message=Constants.GAME_PVE_MSG_MISSING_NAME)
         else:
             if len(spieler_namen[0]) > 0:
                 self.game_window()
             else:
-                messagebox.showwarning(message="Der Spieler 1 hat keinen Namen")
+                messagebox.showwarning(message=Constants.GAME_PVE_MSG_MISSING_NAME)
 
     @staticmethod
-    def action_shutdown_system():
+    def action_shutdown_system() -> None:
+        """
+        Shutdown the running operating system.
+        :return: None
+        """
         print("Shutting down OS")  # todo implement os shutdown function
 
+
 ## todo Die drei Sounds implementieren
+## todo GUI Schick machen... Schrift größer, Hintergrundfarbe f. Spieler setzen
 ## todo In GUI und GameLogic alle Methoden, die nicht von außen gebraucht werden mit __ verstecken
 ## todo Konstanten auslagern
 ## todo Methoden Doku und Kommentare weg
